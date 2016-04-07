8c095a05f5371a02c3aa3934d9d064554b2d4c21
Nesrine <nesrine.chalouah@gmail.com>
2016-04-07 15:13:44 +0200

    intialization of the project Fast IT vitrine

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('siteVitrine', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
